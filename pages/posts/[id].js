47b48b1aa3655f78c8a2e6b77f19feb29bfa5c2b
subin <stnqls3938@gmail.com>
2023-01-16 18:02:27 +0900

    create-next-app ~ api routes

---
+++ pages/posts/[id].js
@@ -0,0 +1,68 @@
+import Head from "next/head";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Layout from "../../components/layout";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  // const router = useRouter();
+  // if (router.isFallback) {
+  //   return <div>Loading</div>;
+  // }
+
+  useEffect(() => {
+    const getText = async () => {
+      const res = await fetch("/api/hello");
+      const data = await res.json();
+
+      alert(data.text);
+    };
+    getText();
+  }, []);
+
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
