9d0022de393d1439a8dac8b6edb67098b260b463
subin <stnqls3938@gmail.com>
2023-01-17 15:43:12 +0900

    getServerSideProps()사용하기

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,37 +1,47 @@
 import Head from "next/head";
-import { getAllPostIds, getPostData } from "../../lib/posts";
+import { getPostData } from "../../lib/posts";
 import Layout from "../../components/layout";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-export async function getStaticProps({ params }) {
+// export async function getStaticProps({ params }) {
+//   const postData = await getPostData(params.id);
+//   return {
+//     props: {
+//       postData,
+//     },
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   const paths = getAllPostIds();
+//   // [
+//   //   {
+//   //     params: {
+//   //       id: 'ssg-ssr'
+//   //     }
+//   //   },
+//   //   {
+//   //     params: {
+//   //       id: 'pre-rendering'
+//   //     }
+//   //   }
+//   // ]
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+
+export async function getServerSideProps({ params, req }) {
+  console.log(`req.cookies:${JSON.stringify(req.cookies)}`);
   const postData = await getPostData(params.id);
   return {
     props: {
       postData,
     },
-  };
-}
-
-export async function getStaticPaths() {
-  const paths = getAllPostIds();
-  // [
-  //   {
-  //     params: {
-  //       id: 'ssg-ssr'
-  //     }
-  //   },
-  //   {
-  //     params: {
-  //       id: 'pre-rendering'
-  //     }
-  //   }
-  // ]
-  return {
-    paths,
-    fallback: false,
   };
 }
 
