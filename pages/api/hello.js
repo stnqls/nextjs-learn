8aac42291af9f6b413aabedab01cead193567b33
subin <stnqls3938@gmail.com>
2023-01-17 21:44:10 +0900

    shallow routing, api redirect

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,5 @@
 export default function handler(req, res) {
-  res.status(200).json({ text: "Hello" });
+  // res.status(200).json({ text: "Hello" });
+  res.redirect(307, "/api/bye");
+  console.log("api/hello");
 }
